d4eff2bef11cf5833c888aaf0aeb20f34911bc25
biswabhusan9 <biswabhusanp121@gmail.com>
2025-07-17 00:20:28 +0530

    fix-patch-issue

--- pages/api/_utils.js
+++ pages/api/_utils.js
@@ -53,9 +53,9 @@ export function setCorsHeaders(req, res) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
 }
 
-export { COOKIE_NAME }; 
+export { COOKIE_NAME };
